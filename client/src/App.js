fbe4c457b927dfec0c89e13e0c35dab95b6cfa5b
Joseph Perry <josephperry720@gmail.com>
2020-11-05 16:09:07 -0500

    navbar links to all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Saved from "./containers/Saved/Saved";
 import Search from "./containers/Search/Search";
+import Navbar from "./components/Navbar/Navbar"
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Switch>
         <Route exact path="/" component={Search} />
         <Route exact path="/saved" compnent={Saved} />
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <Link to="/" class="brand-logo right">
+          Google Books Search
+        </Link>
+        <ul id="nav-mobile" className="left hide-on-med-and-down">
+          <li>
+            <Link to="/">Search</Link>
+          </li>
+          <li>
+            <Link to="/saved">Saved Books</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
