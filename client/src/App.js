fa698a12b62f37f0ef163b284c84f49c27ed8c5b
Joseph Perry <josephperry720@gmail.com>
2020-11-05 15:50:19 -0500

    basic react containers with react router routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
+import Saved from "./containers/Saved/Saved";
+import Search from "./containers/Search/Search";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 function App() {
   return (
-    <h1>This is my app!</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/saved" compnent={Saved} />
+      </Switch>
+    </Router>
   );
 }
 
