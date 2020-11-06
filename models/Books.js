5a95f810d3be0ad6747a75ecf4a3efe48ee1e70b
Joseph Perry <josephperry720@gmail.com>
2020-11-06 15:14:36 -0500

    created Books Schema

---
+++ models/Books.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BooksSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  authors: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+});
+
+const Books = mongoose.model("Books", BooksSchema);
+module.exports = Books;
