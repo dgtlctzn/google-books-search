b9f087a708238f63e6196745697ff00d625f7624
Joseph Perry <josephperry720@gmail.com>
2020-11-07 13:22:45 -0500

    try to rerender page

--- client/src/containers/Saved/Saved.jsx
+++ client/src/containers/Saved/Saved.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
+import {useHistory} from "react-router-dom";
 import API from "../../utils/API";
 import Card from "../../components/Card/Card";
 
+
 const Saved = () => {
+  
   const [savedBooks, setSavedBooks] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     API.getSavedBooks()
@@ -17,8 +21,9 @@ const Saved = () => {
   }, []);
 
   const handleDelete = (e) => {
-    API.deleteBook(e.target.value).then(() => {
-      console.log("book deleted")
+    API.deleteBook(e.target.value).then((data) => {
+      console.log(data);
+      history.push("/saved");
     }).catch(err => {
       console.log(err);
     })
